0060f8842fc09b157c91574c1f8839f509181e77
Shi Tao Luo <sluo5991@gmail.com>
2021-10-10 17:43:55 -0400

    added "new" and "delete" implementation

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ let input = prompt("Enter Command: ");
 let list = ['cookie', 'chicken'];
 
 while (input !== 'quit' && input !== 'q') {
+  /*
+  print current to do list
+  */
   if (input === "list") {
     console.log("**********");
     for (let i = 0; i < list.length; i++) {
@@ -10,6 +13,21 @@ while (input !== 'quit' && input !== 'q') {
     }
     console.log("**********");
   }
+  /*
+  add a new item to do list
+  */
+  if (input === "new") {
+    let newItem = prompt("Enter a item to add: ");
+    list.push(newItem);
+  }
+
+  /*
+  delete a item in to do list with current index
+  */
+  if (input === "delete") {
+    let idxToDelete = prompt("Enter index of item to delete: ");
+    list.splice(idxToDelete, 1);
+  } 
   input = prompt("Enter Command");
 }
 console.log("You quit the app");
